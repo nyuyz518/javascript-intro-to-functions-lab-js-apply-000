4575069235cddd0de4f16ff7fd8984910247d5cd
Lillian Zhang <lilyzhangyun@gmail.com>
2018-04-26 02:10:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string){
   if (string.toLowerCase() == string) {
     return "I can't hear you!";
   }
-  if (string.toUpperCase() == string); {
+  if (string.toUpperCase() == string) {
       return "YES INDEED!";
   }
   

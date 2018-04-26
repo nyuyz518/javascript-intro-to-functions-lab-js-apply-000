49127802df7b47d99d54f7969ba53fe00b1d96f3
Lillian Zhang <lilyzhangyun@gmail.com>
2018-04-26 02:09:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
+  console.log(string == "I love you, Grandma.");
   if (string == "I love you, Grandma."); {
      return "I love you, too.";
   }
